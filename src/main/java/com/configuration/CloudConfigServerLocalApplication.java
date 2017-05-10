2829d125b9307f58b6f216d37d7acc36a178a299
Vasilis Christopoulos <vasilis.christopoulos@oramon.nl>
2017-05-11 00:23:34 +0200

    Initial commit of the server

---
+++ src/main/java/com/configuration/CloudConfigServerLocalApplication.java
@@ -0,0 +1,14 @@
+package com.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigServerLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerLocalApplication.class, args);
+	}
+}
